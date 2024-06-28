fe4b1d5d05e40dd3f14f53dca6ac82b588342593
Keshav Shah <keshavrshah@gmail.com>
2024-06-28 18:22:54 -0400

    vercel fix

--- lib/protectedRoute.ts
+++ lib/protectedRoute.ts
@@ -1,12 +1,10 @@
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 
 export function protectedRoute(){
-    const router = useRouter();
     const { data: session, status } = useSession({
         required: true,
         onUnauthenticated() {
-            router.push("/login");
+            location.href="/"
         },
     });
     return {session, status};
