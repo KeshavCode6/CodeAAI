53939d1e9842b282805d6b7656f4c4addb707fb2
Keshav Shah <keshavrshah@gmail.com>
2024-06-28 22:59:09 -0400

    vercel 6

--- lib/protectedRoute.ts
+++ lib/protectedRoute.ts
@@ -1,15 +1,17 @@
 import { useSession } from "next-auth/react";
-import { Red_Rose } from "next/font/google";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export function useProtectedRoute(){
-    const router = useRouter();
-    const { data: session, status } = useSession({
-        required: true,
-        onUnauthenticated() {
-            router.push("/")
-        },
-    });
-    return {session, status};
+export function useProtectedRoute() {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (status === "loading") return; // Do nothing while loading
+    if (!session) {
+      router.push("/");
+    }
+  }, [session, status, router]);
+
+  return { session, status };
 }
-
