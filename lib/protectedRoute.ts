719039c2b1eba3453c8a0a18eedb54cb1d14861f
Keshav Shah <keshavrshah@gmail.com>
2024-06-28 22:49:58 -0400

    vercel 4

--- lib/protectedRoute.ts
+++ lib/protectedRoute.ts
@@ -1,6 +1,8 @@
 import { useSession } from "next-auth/react";
+import { Red_Rose } from "next/font/google";
 
 export function useProtectedRoute(){
+    if(typeof window == "undefined") return {};
     const { data: session, status } = useSession({
         required: true,
         onUnauthenticated() {
