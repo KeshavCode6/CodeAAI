e88b6de3bd2b054bf4526ee71daf3e38c72d1194
Shaurya Kumar <Shaurya>
2024-08-24 12:29:42 -0400

    cleaned some code up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,92 @@
 "use client"
 import { Button } from "@/components/ui/button"
-import HomeInfoCard from "@/components/custom/landing/HomeInfoCard"
 import Footer from "@/components/custom/general/footer"
 import Navigation from "@/components/custom/general/navigation"
 import Link from "next/link"
-import { faqs, members } from "@/lib/constants"
-import TeamCard from "@/components/custom/landing/TeamCard"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 import ScrollableSection from "@/components/custom/general/scrollableSection"
+import { Card, CardTitle } from "@/components/ui/card"
+import { GlobeIcon } from "lucide-react"
 
 // Landing page
 export default function Landing() {
 
+  const infoCards = [
+    {
+      iconUrl: "/assets/info-card-icons/hackathons.png", 
+      title: "Hackathons", 
+      description: "Participate in exciting hackathons with top-tier developers from around the globe. Collaborate, create, and compete for amazing prizes!"
+    }, 
+    {
+      iconUrl: "/assets/info-card-icons/hackathons.png", 
+      title: "Challenges", 
+      description: "Take on various coding challenges designed to push your skills to the next level. Tackle problems and earn rewards as you grow."
+    }, 
+    {
+      iconUrl: "/assets/info-card-icons/hackathons.png", 
+      title: "Competitions", 
+      description: "Engage in friendly competition with like-minded individuals. Hone your skills and become a leader in your field!"
+    }, 
+    {
+      iconUrl: "/assets/info-card-icons/hackathons.png", 
+      title: "Stay Notified", 
+      description: "Stay informed about the latest events, challenges, and competitions. Never miss an opportunity to shine!"
+    }, 
+  ]
+
+  const teamMembers = [
+    {
+      name: "Keshav Shah",
+      role: "VP of Development",
+      imageUrl: "/assets/team-members/keshav-shah.png",
+      url: "https://keshav.pro",
+      githubUrl: "https://github.com/KeshavCode6",
+    },
+    {
+      name: "Shaurya Kumar",
+      role: "VP of Development",
+      imageUrl: "/assets/team-members/shaurya-kumar.png",
+      url: "https://shaurya.pro",
+      githubUrl: "https://github.com/kshau",
+    },
+  ]
+
+  const faqs = [
+    {
+      question: "What topics do we cover in our tech articles?",
+      answer: "We cover a wide range of topics emegering technology, web development, ai, and more"
+    },
+    {
+      question: "How often do you publish new tutorials?",
+      answer: "We publish 3-4 new tutorials/articles every week!"
+    },
+    {
+      question: "Do you have podcasts, and how frequently are they released?",
+      answer: "Yes, we release podcasts weekly where we discuss the latest trends in technology, interview industry experts, and share valuable insights."
+    },
+    {
+      question: "Can I edit articles if needed?",
+      answer: `<span> Absolutely! We welcome any article revisions <a href="https://github.com/TechSwan/techswan-articles" target="_blank" className="text-blue-400 hover:underline"> on our GitHub</a>.</span>`
+    },
+    {
+      question: "Are your tutorials suitable for everyone?",
+      answer: "Yes, we offer tutorials for all skill levels, from beginners to advanced developers."
+    }
+  ]
+
   return (
-    <Navigation path={"/"}>
+    <Navigation path="/">
       {/* Hero Section */}
-      <div className="h-screen bg-cover flex justify-center flex-col bg-[url('/assets/home/background.png')]">
+      <div className="h-screen bg-cover flex justify-center flex-col bg-[url('/assets/background.png')]">
         <div className=" w-full h-full bg-gradient-to-r from-homeGradientStart backdrop-blur-sm" style={{ backgroundColor: "rgba(16, 12, 52, 0.8)" }}>
           <div className="flex flex-col absolute top-1/2 -translate-y-1/2 self-center mt-[-6rem] ml-40 gap-5">
-            <span className="text-6xl mb-[-0.5rem]">
+            <span className="text-6xl mb-[-0.5rem] font-bold">
               <span> Learn </span>
               <span className="text-[#0083CD]"> code </span>
               <span> by writing </span>
               <span className="text-[#0083CD]">code</span>
             </span>
-            <span className="text-3xl font-light">Brought to you by Keshav Shah and Shaurya Kumar</span>
+            <span className="text-3xl font-light">Brought to you by AAI Coding Club</span>
             <div className="flex flex-row gap-5">
               <Link href="/dashboard">
                 <Button className="w-48 rounded-xl text-white">Play now</Button>
@@ -49,21 +111,24 @@ export default function Landing() {
             </span>
           </div>
           <div className="grid grid-cols-2 gap-2">
-            <HomeInfoCard title="Hackathons" icon="/assets/home/info-card-icon/hackathons.png">
-              Participate in exciting hackathons with top-tier developers from around the globe. Collaborate, create, and compete for amazing prizes!
-            </HomeInfoCard>
 
-            <HomeInfoCard title="Challenges" icon="/assets/home/info-card-icon/hackathons.png">
-              Take on various coding challenges designed to push your skills to the next level. Tackle problems and earn rewards as you grow.
-            </HomeInfoCard>
+            {infoCards.map((infoCard, index) => (
+              <Card className="w-56 aspect-square grow text-center flex items-center justify-center relative transition-all duration-300 hover:scale-105 hover:bg-primary hover:bg-opacity-75 group" key={index}>
+                {/* Icon and Title */}
+                <div className="flex flex-col items-center justify-center gap-2 z-0 transition-opacity duration-300 group-hover:opacity-15">
+                    <img src={infoCard.iconUrl} className="w-32 rounded-full self-center" />
+                    <CardTitle className="text-lg">{infoCard.title}</CardTitle>
+                </div>
 
-            <HomeInfoCard title="Competition" icon="/assets/home/info-card-icon/hackathons.png">
-              Engage in friendly competition with like-minded individuals. Hone your skills and become a leader in your field!
-            </HomeInfoCard>
+                {/* Children (Overlay) */}
+                <div className="absolute inset-0 opacity-0 group-hover:opacity-100 p-4 flex items-center justify-center transition-opacity duration-300 z-20">
+                  <span>
+                    {infoCard.description}
+                  </span>
+                </div>
+              </Card>
+            ))}
 
-            <HomeInfoCard title="Stay Notified" icon="/assets/home/info-card-icon/hackathons.png">
-              Stay informed about the latest events, challenges, and competitions. Never miss an opportunity to shine!
-            </HomeInfoCard>
           </div>
         </ScrollableSection>
       </div>
@@ -73,10 +138,35 @@ export default function Landing() {
         <span id="contact" />
         <ScrollableSection className="flex flex-col items-center  px-4 md:px-8">
           <h2 className="text-3xl md:text-3xl font-bold mb-8 md:mb-12 text-center">Meet Our Team</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-16 mb-20">
-            {members.map((member, index) => (
-              <TeamCard key={index} name={member.name} image={member.image} github={member.github} url={member.url} role={member.role} />
+          <div className="flex flex-wrap gap-8 md:gap-16 mb-20">
+
+            {teamMembers.map((teamMember, index) => (
+              <div className="flex flex-col items-center transition-all duration-300 hover:transform hover:scale-105" key={index}>
+                <div className="w-48 h-48 rounded-full overflow-hidden shadow-lg mb-6">
+                    <img
+                        className="w-full h-full object-cover"
+                        src={teamMember.imageUrl}
+                        alt={teamMember.name}
+                    />
+                </div>
+                <span className="font-bold text-2xl mb-2">{teamMember.name}</span>
+                <span className="font-semibold text-lg text-primary text-center">{teamMember.role}</span>
+                <div className="mt-4 flex gap-4">
+                    {teamMember.url ? (
+                        <a target="_blank" href={teamMember.url} className="text-gray-600 hover:text-primary">
+                            <GlobeIcon />
+                        </a>
+                    ) : <></>}
+    
+                    <a target="_blank" href={teamMember.githubUrl} className="text-gray-600 hover:text-primary">
+                        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
+                        </svg>
+                    </a>
+                </div>
+              </div>
             ))}
+
           </div>
         </ScrollableSection>
       </div>
@@ -115,7 +205,7 @@ export default function Landing() {
           </div>
         </ScrollableSection>
 
-        <img src="/assets/home/join-now.png" className="w-full" />
+        <img src="/assets/join-now.png" className="w-full" />
 
       </div>
 
