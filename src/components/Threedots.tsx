4ffbf73eae68e6294a595075fdefbab9bf0b98ac
Keshav Shah <keshavrshah@gmail.com>
2024-08-24 21:13:46 -0400

    lil metro

--- src/components/Threedots.tsx
+++ src/components/Threedots.tsx
@@ -4,7 +4,7 @@ export function ThreeDots() {
         <div className="flex space-x-1">
             <div className="w-1 h-1 bg-white rounded-full animate-loading-dot" />
             <div className="w-1 h-1 bg-white rounded-full animate-loading-dot delay-200" />
-            <div className="w-1 h-1 bg-white rounded-full animate-loading-dot" />
+            <div className="w-1 h-1 bg-white rounded-full animate-loading-dot delay-300" />
         </div>
     );
 }
